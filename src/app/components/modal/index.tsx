07aa0f23f5d640556368a5f378258a8c0a7ef163
Hiroya Nakano <hiroya_0610_15mro@me.com>
2023-06-17 19:00:00 +0900

    :sparkler: Iframe操作機能追加

--- src/app/components/modal/index.tsx
+++ src/app/components/modal/index.tsx
@@ -5,7 +5,10 @@ import { useRef } from 'react'
 
 export function Modal():JSX.Element | null {
   const dialogRef: React.MutableRefObject<HTMLDialogElement | null> = useRef(null);
-  console.log(dialogRef.current);
+  const iframeEl = dialogRef.current?.querySelector('iframe');
+
+  /** iframe要素のwindowオブジェクトを取得 */
+  const targetWidow = iframeEl?.contentWindow;
 
   if(!dialogRef) {
     return null;
@@ -15,14 +18,24 @@ export function Modal():JSX.Element | null {
     dialogRef.current?.showModal();
   }
 
+  /** iframeの動画を操作する関数 */
+  const videoControll = (action: string, arg=null) => {
+    targetWidow?.postMessage('{"event":"command", "func":"'+action+'", "args":'+arg+'}', '*');
+  }
+
   const closeModal = ():void => {
     dialogRef.current?.close();
+    videoControll('pauseVideo');
+    
   }
 
   const stopPropagation = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
     e.stopPropagation();
   }
+
+  console.log(dialogRef.current);
   
+
   return (
     <div>
       <div>
@@ -35,7 +48,7 @@ export function Modal():JSX.Element | null {
             </div>
             <div className="relative">
               <p>エイペックス-Season6-トレーラー</p>
-              <iframe width="560" height="315" src="https://www.youtube.com/embed/qzdGo7FBndA" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"></iframe>
+              <iframe width="560" height="315" src="https://www.youtube.com/embed/qzdGo7FBndA?enablejsapi=1&origin=https://my-portfolio-nine-omega-74.vercel.app" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"></iframe>
               <div className="absolute -top-[25px] right-[0px]">
                 <button onClick={closeModal} className="block w-12 h-12 relative before:content-[''] before:block before:w-full before:h-1 before:bg-black before:origin-[0%_50%] before:rotate-45 after:content-[''] before:absolute before:left-[14%] before:top-[calc(14%_-_5px)] after:block after:w-full after:h-1 after:bg-black after:-rotate-45 after:origin-[100%_50%] after:right-[14%] after:absolute after: left-auto after:top-[calc(14%_-_5px)]">
                     <span className="sr-only">閉じる</span>
